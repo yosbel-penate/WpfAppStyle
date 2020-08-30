e0a9b62a4f314c7cbf2b00863958040dabefb390
yosbel-penate <58090876+yosbel-penate@users.noreply.github.com>
2020-08-30 03:24:20 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppStyle
+{
+   
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Person> persons = new List<Person>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                Random a = new Random();
+                persons.Add(new Person($"Pepe{i}", a.Next(5, 70), a.NextDouble() * 100));
+            }
+            dtGrid.ItemsSource = persons;
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppStyle
+{
+    class Person
+    {
+        string name;
+        int age;
+        double salary;
+
+        public Person(string name, int age, double salary)
+        {
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public double Salary { get => salary; set => salary = value; }
+    }
+}
